165f1119d19d8108693b5f0c12f2d8ceedad4d44
ViniciusYogo <viniciusyogo@gmail.com>
2024-09-03 20:39:44 -0300

    primeiroEnvio
    Primeiro exercício pronto

---
+++ exercicios/exe1/nota.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+    <form>
+
+        <?php
+
+        $nome = $_POST['inputNome'];
+        $email = $_POST["inputEmail"];
+        $idade = $_POST["inputIdade"];
+
+        if ($idade >= 18) {
+            echo ("O seu nome é $nome <br>" );
+
+            echo ("O sua idade é $idade <br>");
+
+            echo ("O seu email é $email");
+        } else {
+            echo ("Não pode se cadastrar");
+        }
+
+
+        ?>
+    </form>
+</body>
+
+</html>
