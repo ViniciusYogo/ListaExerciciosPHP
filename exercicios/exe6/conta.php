7b56ce8f34d7215f32cb8a6bfd58ccc8136011d6
ViniciusYogo <viniciusyogo@gmail.com>
2024-09-06 21:22:14 -0300

    exe6
    exercicio 6 quase pronto

--- exercicios/exe6/conta.php
+++ exercicios/exe6/conta.php
@@ -1,23 +1,38 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="style.css">
 </head>
-<body>
-    <?php 
-    $opcao=$_POST["opcao"];
-    $saldo =0 ;
-    
-    switch ($opcao) {
-        case "1":
-            echo("<h1>O seu saldo é de: R$ $saldo</h1>");
-            break;
-        case "2":
-            
-        }
 
-?>
+<body>
+    <div>
+
+        <?php
+        $valor = $_POST["valor"];
+        $opcao = $_POST["opcao"];
+        
+
+        switch ($opcao) {
+            case "1":
+                echo ("<h1>O seu saldo é de: R$ $saldo</h1>");
+
+                break;
+            case "2":
+                $saldo += $valor;
+                echo ("<h1>O valor depositado foi de $valor</h1> <br>");
+                echo ("<h1>O seu saldo é de: R$ $saldo</h1>");
+                break;
+        }
+        ?>
+
+        <button><a href="index.html">Voltar</a></button>
+    </div>
+
+
 </body>
+
 </html>
