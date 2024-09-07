36e31b72b89262f73cde0948c2b7edd78ebc1c0a
ViniciusYogo <viniciusyogo@gmail.com>
2024-09-06 21:30:12 -0300

    teste
    teste exe6

--- exercicios/exe6/conta.php
+++ exercicios/exe6/conta.php
@@ -9,13 +9,40 @@
 </head>
 
 <body>
+<form action="conta.php" method="post">
+        <h1>Conta</h1>
+        <br>
+        <select id="opcao" name="opcao" onchange="escolha()">
+            <option disabled selected>Escolha uma opção</option>
+            <option value=1>1- Ver Saldo</option>
+            <option value=2>2- Depositar</option>
+            <option value=3>3- Sacar</option>
+            <option value=4>4- Sair</option>
+        </select>
+        <br>
+        <input type="number" class="valor" id="valor" name="valor">
+        <br>
+        <button type="submit">Escolher</button>
+    </form>
+
+    <script>
+        function escolha() {
+            var opcao = document.getElementById('opcao').value;
+
+            if(opcao == 2 || opcao == 3){
+                var valor = document.getElementById('valor').style.opacity=1
+            }
+            else{
+                var valor = document.getElementById('valor').style.opacity=0
+            }
+        }
+    </script>
     <div>
 
         <?php
         $valor = $_POST["valor"];
         $opcao = $_POST["opcao"];
         
-
         switch ($opcao) {
             case "1":
                 echo ("<h1>O seu saldo é de: R$ $saldo</h1>");
